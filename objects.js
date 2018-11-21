5beb319abb2fbaa5e17ba09e49b7d01972c23869
Ted Sazon <tedsazon@gmail.com>
2018-11-21 17:07:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
-var playlist = { artistName: 'songTitle' } 
+var playlist = { artistName: 'songTitle' }
+
+function updatePlaylist(playlist, 'artistName', 'songTitle') {
+  return playlist['artistName'] = 'songTitle'
+}
